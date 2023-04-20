4db729a1c230f984dc9318fb5041625e66dd151a
RobusV <rverplanke@gmail.com>
2023-04-20 15:31:40 +0200

    create 'place maker' functionality

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,21 @@ const Player = (name, symbol) => ({ name, symbol });
 
 
 const GameBoard = (() => {
-    const board = ['X','O','O','X','O','X','X','O','X'];
+    const board = ['','','','','','','','',''];
 
     const placeMarker = (index, symbol) => {
+        const newBoard = board;
+        newBoard[index] = symbol;
+        DisplayController.resetBoard();
+        DisplayController.printBoard();
+        console.log('placeMarker called');
+        console.log(newBoard);
+
+
         // const availableCells = board.filter();
         // availableCells[index] = symbol;
+
+
     };
 
     const checkForWin = () => {
@@ -19,24 +29,44 @@ const GameBoard = (() => {
 
 const DisplayController = (() => {
 
-    // show display on screen - create a button for each array item
-    const printBoard = () => {
-        GameBoard.board.forEach( cell => { 
-            const displayBoard = document.querySelector('#grid-container');
-            const printCell = document.createElement('button');
-            displayBoard.append(printCell);
-            printCell.classList.add('cell');
-            printCell.innerText = cell;
-        });
-    }
+    // create a button on the grid, set a class, get marker symbol and add event listener
+    const createCell = (cell) => {
+        const displayBoard = document.querySelector('#grid-container');
+        const displayCell = document.createElement('button');
+        displayBoard.append(displayCell);
+        displayCell.className = 'cell';
+        displayCell.innerText = cell;
+        displayCell.addEventListener('click', (e) => GameBoard.placeMarker(e, 'x'));
+    };
 
+    // clear board
+    const resetBoard = () => {
+        const displayBoard = document.querySelector('#grid-container');
+        displayBoard.innerHTML = '';
+        console.log('resetBoard called');
+    };
+
+    // show display on screen
+    const printBoard = () => {
+        GameBoard.board.forEach(cell => createCell(cell));
+        console.log('printBoard called');
+
+    };
+
+    // update board
+    // const updateBoard = () => {
+
+    //     GameBoard.board.forEach(cell => createCell(cell));
+
+    // };
+    
     // show who is winner / tie
     const showResult = () => {
         const displayMsg = document.querySelector('#game-display')
 
     }
 
-    return { printBoard, showResult};
+    return { resetBoard, printBoard, showResult};
 })();
 
 DisplayController.printBoard();
