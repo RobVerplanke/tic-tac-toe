fbd40b0a2cf635aac7c44e156588c3a203519ccd
RobusV <rverplanke@gmail.com>
2023-04-20 16:07:26 +0200

    reorganize methods

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,15 @@ const Player = (name, symbol) => ({ name, symbol });
 const GameBoard = (() => {
     const board = ['','','','','','','','',''];
 
+    // checks wether cell is empty or not
+    const checkAvailableCell = (index) => {
+        // if board[index] !== '' return false
+    };
+    
     const placeMarker = (index, symbol) => {
-        const newBoard = board;
-        newBoard[index] = symbol;
-        DisplayController.resetBoard();
-        DisplayController.printBoard();
-        console.log('placeMarker called');
-        console.log(newBoard);
-
-
-        // const availableCells = board.filter();
-        // availableCells[index] = symbol;
-
-
+        checkAvailableCell(index);
+        // if checkAvailableCell === true > board[index] = symbol
+        DisplayController.updateBoard();
     };
 
     const checkForWin = () => {
@@ -31,45 +27,31 @@ const DisplayController = (() => {
 
     // create a button on the grid, set a class, get marker symbol and add event listener
     const createCell = (cell) => {
-        const displayBoard = document.querySelector('#grid-container');
         const displayCell = document.createElement('button');
-        displayBoard.append(displayCell);
         displayCell.className = 'cell';
         displayCell.innerText = cell;
         displayCell.addEventListener('click', (e) => GameBoard.placeMarker(e, 'x'));
+        
+        const displayBoard = document.querySelector('#grid-container');
+        displayBoard.append(displayCell);
     };
 
-    // clear board
-    const resetBoard = () => {
+    // clear current board and create new cell for each array item
+    const updateBoard = () => {
         const displayBoard = document.querySelector('#grid-container');
         displayBoard.innerHTML = '';
-        console.log('resetBoard called');
-    };
-
-    // show display on screen
-    const printBoard = () => {
         GameBoard.board.forEach(cell => createCell(cell));
-        console.log('printBoard called');
-
     };
-
-    // update board
-    // const updateBoard = () => {
-
-    //     GameBoard.board.forEach(cell => createCell(cell));
-
-    // };
     
     // show who is winner / tie
     const showResult = () => {
         const displayMsg = document.querySelector('#game-display')
+    };
 
-    }
-
-    return { resetBoard, printBoard, showResult};
+    return { updateBoard, showResult};
 })();
 
-DisplayController.printBoard();
+DisplayController.updateBoard();
 
 const GameController = {
     currentPlayer: null,
