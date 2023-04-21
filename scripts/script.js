d5489959e243e44b02f35f8146348cea5a10442b
RobusV <rverplanke@gmail.com>
2023-04-21 17:54:46 +0200

    change active player after a valid move. reorder functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,46 @@
 /* eslint-disable no-plusplus */
-const Player = (name, symbol) => ({ name, symbol });
-
 const GameBoard = (() => {
     const board = ['','','','X','','O','','',''];
 
-    // checks wether cell is empty or not
+    // checks wether selected cell is empty or not
     const checkAvailableCell = (index) => {
         if (board[index] !== '') {
             console.log('already taken');
             return false;
-        };
-    };
-    
-    const placeMarker = (e, index, symbol) => {
-        console.log(checkAvailableCell(index));
-        if (checkAvailableCell(index) !== true) board[index] = symbol;
-        DisplayController.updateBoard();
+        }
+        return true;
     };
 
     const checkForWin = () => {
         // check for win
     };
+    
+    // if selected cell is empty, place a marker
+    const placeMarker = (index, marker) => {
+        if (checkAvailableCell(index) === true) {
+            board[index] = marker;
+            // checkForWin();
+            console.log(GameController.currentPlayer);
+            GameController.switchPlayers();
+        };
+        
+        DisplayController.updateBoard();
+    };
 
-    return { board, placeMarker, checkForWin };
+    return { board, checkAvailableCell, placeMarker, checkForWin };
 })();
-
 
 const DisplayController = (() => {
 
-    // create a button on the grid, set a class, get marker symbol and add event listener
+    // create a button on the grid and set a class, inner text, data attribute,
+    // marker symbol and a event listener for each one
     const createCell = (cell, index) => {
-        console.log(index);
 
         const displayCell = document.createElement('button');
         displayCell.className = 'cell';
         displayCell.innerText = cell;
         displayCell.setAttribute('data', index);
-        displayCell.addEventListener('click', (e) => GameBoard.placeMarker(e, index, 'x'));
+        displayCell.addEventListener('click', () => GameController.placeMarker(index));
         
         const displayBoard = document.querySelector('#grid-container');
         displayBoard.append(displayCell);
@@ -60,12 +64,33 @@ const DisplayController = (() => {
     return { updateBoard, showResult};
 })();
 
-DisplayController.updateBoard();
+const Player = (name, marker) => ({ name, marker });
+const player1 = Player('Player 1', 'X');
+const player2 = Player('Player 2', 'O');
 
-const GameController = {
-    currentPlayer: null,
-
+const GameController = (() => {
+    let currentPlayer = player1;
+    
     // startGame()
-    // switchPlayers
     // handlePlayerMove()
-};
+
+    const switchPlayers = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    };
+    
+     // if selected cell is empty, place a marker
+     const placeMarker = (index) => {
+        if (GameBoard.checkAvailableCell(index) === true) {
+            GameBoard.board[index] = currentPlayer.marker;
+            // checkForWin();
+            console.log(currentPlayer);
+            switchPlayers();
+        };
+        
+        DisplayController.updateBoard();
+    };
+
+    return { currentPlayer, placeMarker, switchPlayers };
+})();
+
+DisplayController.updateBoard();
