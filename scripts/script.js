e54247c78373ce29dfc640ee15ba98cfc7eb93e2
RobusV <rverplanke@gmail.com>
2023-04-21 19:53:31 +0200

    rename variables, clean up code

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ const GameBoard = (() => {
 
     // checks wether selected cell is empty or not
     const checkAvailableCell = (index) => {
+        
         if (board[index] !== '') {
             console.log('already taken');
             return false;
@@ -17,6 +18,7 @@ const GameBoard = (() => {
     
     // if selected cell is empty, place a marker
     const placeMarker = (index, marker) => {
+        
         if (checkAvailableCell(index) === true) {
             board[index] = marker;
             // checkForWin();
@@ -24,36 +26,41 @@ const GameBoard = (() => {
             GameController.switchPlayers();
         };
         
-        DisplayController.updateBoard();
+        DisplayController.updateDisplay();
     };
 
     return { board, checkAvailableCell, placeMarker, checkForWin };
 })();
 
+
 const DisplayController = (() => {
 
-    // create a button on the grid and set a class, inner text, data attribute,
-    // marker symbol and a event listener for each one
-    const createCell = (cell, index) => {
+    // create a cell (button) on the display
+    const createCell = (boardItem, index) => {
+        const displayCell = document.createElement('button');  
+        const displayBoard = document.querySelector('#grid-container');
 
-        const displayCell = document.createElement('button');
-        displayCell.className = 'cell';
-        displayCell.innerText = cell;
+        displayBoard.append(displayCell);
+        setCell(displayCell, 'cell', boardItem, index);
+    };
+    
+    // set properties of the newly created cell
+    const setCell = (displayCell, className, boardItem, index)=> {
+        displayCell.className = className;
+        displayCell.innerText = boardItem;
         displayCell.setAttribute('data', index);
         displayCell.addEventListener('click', () => GameController.placeMarker(index));
-        
-        const displayBoard = document.querySelector('#grid-container');
-        displayBoard.append(displayCell);
     };
 
     // clear current board and create new cell for each array item
-    const updateBoard = () => {
-        let index = -1; // this way the counter starts at zero
+    const updateDisplay = () => {
         const displayBoard = document.querySelector('#grid-container');
+        let index = -1; // this way the counter starts at zero
+        
         displayBoard.innerHTML = '';
-        GameBoard.board.forEach(cell => {
+        GameBoard.board.forEach(boardItem => {
             index++;
-            createCell(cell, index)});
+            createCell(boardItem, index)});
     };
     
     // show who is winner / tie
@@ -61,12 +68,14 @@ const DisplayController = (() => {
         const displayMsg = document.querySelector('#game-display')
     };
 
-    return { updateBoard, showResult};
+    return { updateDisplay, showResult};
 })();
+
 
 const Player = (name, marker) => ({ name, marker });
 const player1 = Player('Player 1', 'X');
 const player2 = Player('Player 2', 'O');
+
 
 const GameController = (() => {
     let currentPlayer = player1;
@@ -78,19 +87,22 @@ const GameController = (() => {
         currentPlayer = currentPlayer === player1 ? player2 : player1;
     };
     
+    const getPlayerNamer = () => currentPlayer.name;
+
+    const getPlayerMarker = () => currentPlayer.marker;
+
      // if selected cell is empty, place a marker
      const placeMarker = (index) => {
         if (GameBoard.checkAvailableCell(index) === true) {
-            GameBoard.board[index] = currentPlayer.marker;
+            GameBoard.board[index] = getPlayerMarker();
             // checkForWin();
-            console.log(currentPlayer);
             switchPlayers();
         };
         
-        DisplayController.updateBoard();
+        DisplayController.updateDisplay();
     };
 
     return { currentPlayer, placeMarker, switchPlayers };
 })();
 
-DisplayController.updateBoard();
+DisplayController.updateDisplay();
