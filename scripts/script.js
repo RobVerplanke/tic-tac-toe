079dd3b44963d4d11f13733f0db83ce4ad261743
RobusV <rverplanke@gmail.com>
2023-04-28 19:41:49 +0200

    rewrite display functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
+/* eslint-disable prefer-const */
 /* eslint-disable no-plusplus */
 const GameBoard = (() => {
-    const board = ['','','','X','','O','','',''];
+    const board = ['','','','','','','','',''];
 
     // checks wether selected cell is empty or not
     const checkAvailableCell = (index) => {
@@ -20,88 +21,100 @@ const GameBoard = (() => {
             (board[2] === board[5] && board[2] === board[8] && (!(board[2] === ''))) ||
             (board[0] === board[4] && board[0] === board[8] && (!(board[0] === ''))) ||
             (board[2] === board[4] && board[2] === board[6] && (!(board[2] === ''))) )
-        {
-            DisplayController.showResult();
+        {  
+            console.log(`${GameController.getPlayerName()} is the winner!`);
+            return;
         }
-    };
-    
+
+        if(!(board.includes(''))){
+            console.log('It\'s a tie!');
+        }        
+    };    
 
     return { board, checkAvailableCell, checkForWin };
 })();
 
 
-const DisplayController = (() => {
-
-    // create a cell (button) on the display
-    const createCell = (boardItem, index) => {
-        const displayCell = document.createElement('button');  
-        const displayBoard = document.querySelector('#grid-container');
-
-        displayBoard.append(displayCell);
-        setCell(displayCell, 'cell', boardItem, index);
-    };
-    
-    // set properties of the newly created cell
-    const setCell = (displayCell, className, boardItem, index)=> {
-        displayCell.className = className;
-        displayCell.innerText = boardItem;
-        displayCell.setAttribute('data', index);
-        displayCell.addEventListener('click', () => GameController.placeMarker(index));
-    };
-
-    // clear current board and create new cell for each array item
-    const updateDisplay = () => {
-        const displayBoard = document.querySelector('#grid-container');
-        let index = -1; // this way the counter starts at zero
-        
-        displayBoard.innerHTML = '';
-        GameBoard.board.forEach(boardItem => {
-            index++;
-            createCell(boardItem, index)});
-    };
-    
-    // show who is winner / tie
-    const showResult = () => {
-        const displayMsg = document.querySelector('#game-display')
-        displayMsg.innerHTML = `<p>${GameController.getPlayerNamer()} WINS!</p>`;
-        displayMsg.classList.add('winning-player');
-    };
-
-    return { updateDisplay, showResult};
-})();
-
+// ***** Player object *****
 
 const Player = (name, marker) => ({ name, marker });
 const player1 = Player('Player 1', 'X');
 const player2 = Player('Player 2', 'O');
 
 
+// ***** Game controller *****
+
 const GameController = (() => {
     let currentPlayer = player1;
-    
-    // startGame()
-    // handlePlayerMove()
+
+    const newGame = () =>{
+        DisplayController.updateGrid();
+        DisplayController.updateTurn();
+    };
 
     const switchPlayers = () => {
         currentPlayer = currentPlayer === player1 ? player2 : player1;
+        // DisplayController.updateMessage();
     };
     
-    const getPlayerNamer = () => currentPlayer.name;
+    const getPlayerName = () => currentPlayer.name;
 
     const getPlayerMarker = () => currentPlayer.marker;
 
-     // if selected cell is empty, place a marker
-     const placeMarker = (index) => {
+    // if selected cell is empty, place a marker
+    const placeMarker = (index) => {
         if (GameBoard.checkAvailableCell(index) === true) {
             GameBoard.board[index] = getPlayerMarker();
             GameBoard.checkForWin();
             switchPlayers();
+            DisplayController.updateGrid();
+            DisplayController.updateTurn();
         };
-        
-        DisplayController.updateDisplay();
     };
 
-    return { currentPlayer, getPlayerNamer, placeMarker, switchPlayers };
+    return { currentPlayer, newGame, getPlayerName, placeMarker, switchPlayers };
 })();
 
-DisplayController.updateDisplay();
+
+// ***** Display controller *****
+
+const DisplayController = (() => {
+    const displayBoard = document.querySelector('#grid-container');
+    const displayMessage = document.querySelector('#game-display');
+
+    
+    // set properties of the newly created cell
+    const setCell = (displayCell, className, boardItem, index)=> {
+        const newCell = displayCell;
+        newCell.className = className;
+        newCell.innerText = boardItem;
+        newCell.setAttribute('data', index);
+        newCell.addEventListener('click', () => GameController.placeMarker(index));
+    };
+
+    // create a cell (button) on the display
+    const createCell = (boardItem, index) => {
+        const displayCell = document.createElement('button');  
+        displayBoard.append(displayCell);
+        setCell(displayCell, 'cell', boardItem, index);
+    };
+
+    // Show who's turn it is
+    const updateTurn = () => {
+        displayMessage.innerHTML = `<p>${GameController.getPlayerName()}'s turn</p>`;
+    };
+
+    // clear current board and create new cell for each array item
+    const updateGrid = () => {
+        let index = -1; // this way the counter starts at zero
+        displayBoard.innerHTML = '';
+        GameBoard.board.forEach(boardItem => {
+            index++;
+            createCell(boardItem, index)
+        });
+    };
+    
+    return { updateTurn, updateGrid};
+})();
+
+GameController.newGame();
